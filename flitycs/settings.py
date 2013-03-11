e23b556a611895ed32a079f028c5020bbbdec568
Jacqueline Xu <jxu703@gmail.com>
2013-03-11 16:30:24 -0700

    data

--- flitycs/settings.py
+++ flitycs/settings.py
@@ -107,8 +107,9 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/data/templates'
     'C:/Users/Jacqueline/documents/projects/flitycs/data/templates'
-    'data/templates'
+
 )
 
 INSTALLED_APPS = (
