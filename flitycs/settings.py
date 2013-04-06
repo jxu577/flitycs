524b0acc8ea326f88efa7d43745f8ebb10f31ddf
Kenneth Lin <kenlin@berkeley.edu>
2013-04-06 11:45:58 -0700

    changed absolute dir to relative loll

--- flitycs/settings.py
+++ flitycs/settings.py
@@ -67,7 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'C:/Users/Jacqueline/documents/projects/flitycs/static',
+    'static',
 )
 
 # List of finder classes that know how to find static files in
@@ -115,8 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/data/templates',
-    'C:/Users/Jacqueline/documents/projects/flitycs/data/templates',
+    'data/templates',
 
 )
 
